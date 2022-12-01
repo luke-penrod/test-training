e2234a29beda905b8fef990e873b6a1e5a3c09b8
Luke Penrod <penrod@adobe.com>
2022-12-01 10:11:25 -0700

    analytics-implementation-first-try

---
+++ MyVariables.js
@@ -0,0 +1,10 @@
+var s_account = "adobecx1luke-report-suite";
+var s = s_gi(s_account);
+
+s.trackingServer = "luke.data.adobedc.net";
+
+s.pageName = "Luke's home page";
+s.eVar1 = "Example value";
+s.prop1 = "Prop";
+
+s.t();
