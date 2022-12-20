57a783c2593da73c29f0a95839ca36a921fbf3ef
Luke Penrod <penrod@adobe.com>
2022-12-20 10:13:30 -0700

    updates for websdk implementation

--- another-page-variables.js
+++ another-page-variables.js
@@ -1,7 +1,25 @@
 alloy("configure", {
-
     "edgeConfigId": "2e40f92e-e474-44ed-a69c-bbc7807443a2",
-
     "orgId": "53A16ACB5CC1D3760A495C99@AdobeOrg"
+});
 
+alloy("sendEvent", {
+    "xdm": {
+        "web": {
+            "webPageDetails": {
+                "URL": window.document.URL,
+                "name": window.document.title
+            }
+        },
+        "_experience": {
+            "analytics": {
+                "customDimensions": {
+                    "eVars": {
+                        "eVar1": "Luke Web SDK",
+                        "eVar2": "Christmas tree"
+                    }
+                }
+            }
+        }
+    }
 });
